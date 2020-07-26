9f0c8c9b118646782f7bd73117986eda6b01baf7
yangy <yangy@ufen.cn>
2020-07-26 17:56:27 +0800

    配置中心服务端修改

--- config-server/src/main/java/com/ilidan/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/com/ilidan/configserver/ConfigServerApplication.java
@@ -3,9 +3,11 @@ package com.ilidan.configserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableConfigServer
+@RestController
 public class ConfigServerApplication {
 
     public static void main(String[] args) {
